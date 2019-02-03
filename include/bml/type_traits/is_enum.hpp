783b1062a3df37fbbd775c94adcb6f08636dfaa0
Wee Loong Kuan <kuanweeloong@users.noreply.github.com>
2019-02-03 23:44:49 +0800

    Add is_enum

---
+++ include/bml/type_traits/is_enum.hpp
@@ -0,0 +1,25 @@
+//
+// Copyright (c) 2019 Wee Loong Kuan
+//
+// This file is licensed under the MIT license. For more details, see the LICENSE.md file in the
+// top-level directory of this distribution.
+//
+
+#pragma once
+#include "../detail/macros/compiler_specific_file.hpp"
+#include BML_COMPILER_SPECIFIC_FILE(detail/compiler, is_enum.hpp)
+
+namespace bml
+{
+    //
+    // See std::is_enum.
+    //
+    template <typename T>
+    struct is_enum : detail::is_enum_detail::impl<T>{};
+
+    //
+    // See std::is_enum_v.
+    //
+    template <typename T>
+    inline constexpr auto is_enum_v = bool(is_enum<T>::value);
+}
---
+++ tests/test_cases/type_traits/is_enum.cpp
@@ -0,0 +1,102 @@
+//
+// Copyright (c) 2019 Wee Loong Kuan
+//
+// This file is licensed under the MIT license. For more details, see the LICENSE.md file in the
+// top-level directory of this distribution.
+//
+
+#include <bml_testbench.hpp>
+#include <bml/type_traits/is_enum.hpp>
+
+template <typename T>
+auto check_enum() noexcept -> void
+{
+    static_assert(bml::is_enum<T>::value);
+    static_assert(bml::is_enum_v<T>);
+    
+    static_assert(bml::is_enum<T const>::value);
+    static_assert(bml::is_enum_v<T const>);
+    
+    static_assert(bml::is_enum<T volatile>::value);
+    static_assert(bml::is_enum_v<T volatile>);
+    
+    static_assert(bml::is_enum<T const volatile>::value);
+    static_assert(bml::is_enum_v<T const volatile>);
+}
+
+template <typename T>
+auto check_not_enum() noexcept -> void
+{
+    static_assert(!bml::is_enum<T>::value);
+    static_assert(!bml::is_enum_v<T>);
+    
+    static_assert(!bml::is_enum<T const>::value);
+    static_assert(!bml::is_enum_v<T const>);
+    
+    static_assert(!bml::is_enum<T volatile>::value);
+    static_assert(!bml::is_enum_v<T volatile>);
+    
+    static_assert(!bml::is_enum<T const volatile>::value);
+    static_assert(!bml::is_enum_v<T const volatile>);
+}
+
+enum classic_enum
+{
+    classic_enum_one,
+    classic_enum_two
+};
+
+auto test_main() noexcept -> int
+{
+    // Check that the result is true when the input is an enum type.
+    {
+        check_enum<bmltb::enum_class>();
+        check_enum<classic_enum>();
+    }
+    
+    // Check that the result is false when the input is not an enum type.
+    {
+        check_not_enum<bmltb::enum_class*>();
+        check_not_enum<bmltb::enum_class&>();
+        check_not_enum<bmltb::enum_class const&>();
+        check_not_enum<bmltb::enum_class&&>();
+        check_not_enum<bmltb::enum_class volatile*>();
+        check_not_enum<bmltb::enum_class(&)[]>();
+        check_not_enum<bmltb::enum_class(*)[]>();
+        check_not_enum<bmltb::enum_class[]>();
+        check_not_enum<bmltb::enum_class[3]>();
+        check_not_enum<bmltb::enum_class[][2]>();
+        
+        check_not_enum<classic_enum*>();
+        check_not_enum<classic_enum&>();
+        check_not_enum<classic_enum const&>();
+        check_not_enum<classic_enum&&>();
+        check_not_enum<classic_enum volatile*>();
+        check_not_enum<classic_enum(&)[]>();
+        check_not_enum<classic_enum(*)[]>();
+        check_not_enum<classic_enum[]>();
+        check_not_enum<classic_enum[3]>();
+        check_not_enum<classic_enum[][2]>();
+        
+        check_not_enum<void>();
+        check_not_enum<int>();
+        check_not_enum<float>();
+        
+        check_not_enum<bmltb::class_type>();
+        check_not_enum<int bmltb::class_type::*>();
+        check_not_enum<bmltb::incomplete_class>();
+        check_not_enum<bmltb::union_type[]>();
+        check_not_enum<bmltb::incomplete_class*>();
+        check_not_enum<bmltb::incomplete_class*[][2]>();
+        
+        check_not_enum<auto (int) -> void>();
+        check_not_enum<auto (int) const && noexcept -> void>();
+        check_not_enum<auto (&)(int) -> void>();
+        check_not_enum<auto (*)(int) noexcept -> void>();
+        check_not_enum<auto (*&&)(int) noexcept -> void>();
+        check_not_enum<auto (bmltb::class_type::*)() -> void>();
+        check_not_enum<auto (bmltb::class_type::*)() const volatile && noexcept -> void>();
+    }
+
+    return 0;
+}
