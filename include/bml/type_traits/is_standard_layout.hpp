9e9b7ff58dfdd7d4af316bf9354264f226931b45
Wee Loong Kuan <kuanweeloong@users.noreply.github.com>
2019-02-06 14:32:46 +0800

    Add is_standard_layout

---
+++ include/bml/type_traits/is_standard_layout.hpp
@@ -0,0 +1,25 @@
+//
+// Copyright (c) 2019 Wee Loong Kuan
+//
+// This file is licensed under the MIT license. For more details, see the LICENSE.md file in the
+// top-level directory of this distribution.
+//
+
+#pragma once
+#include "../detail/macros/compiler_specific_file.hpp"
+#include BML_COMPILER_SPECIFIC_FILE(detail/compiler, is_standard_layout.hpp)
+
+namespace bml
+{
+    //
+    // See std::is_standard_layout.
+    //
+    template <typename T>
+    struct is_standard_layout : detail::is_standard_layout_detail::impl<T>{};
+
+    //
+    // See std::is_standard_layout_v.
+    //
+    template <typename T>
+    inline constexpr auto is_standard_layout_v = bool(is_standard_layout<T>::value);
+}
---
+++ tests/test_cases/type_traits/is_standard_layout.cpp
@@ -0,0 +1,105 @@
+//
+// Copyright (c) 2019 Wee Loong Kuan
+//
+// This file is licensed under the MIT license. For more details, see the LICENSE.md file in the
+// top-level directory of this distribution.
+//
+
+#include <bml_testbench.hpp>
+#include <bml/type_traits/is_standard_layout.hpp>
+
+template <typename T>
+auto check_standard_layout() noexcept -> void
+{
+    static_assert(bml::is_standard_layout<T>::value);
+    static_assert(bml::is_standard_layout_v<T>);
+    
+    static_assert(bml::is_standard_layout<T const>::value);
+    static_assert(bml::is_standard_layout_v<T const>);
+    
+    static_assert(bml::is_standard_layout<T volatile>::value);
+    static_assert(bml::is_standard_layout_v<T volatile>);
+    
+    static_assert(bml::is_standard_layout<T const volatile>::value);
+    static_assert(bml::is_standard_layout_v<T const volatile>);
+}
+
+template <typename T>
+auto check_not_standard_layout() noexcept -> void
+{
+    static_assert(!bml::is_standard_layout<T>::value);
+    static_assert(!bml::is_standard_layout_v<T>);
+    
+    static_assert(!bml::is_standard_layout<T const>::value);
+    static_assert(!bml::is_standard_layout_v<T const>);
+    
+    static_assert(!bml::is_standard_layout<T volatile>::value);
+    static_assert(!bml::is_standard_layout_v<T volatile>);
+    
+    static_assert(!bml::is_standard_layout<T const volatile>::value);
+    static_assert(!bml::is_standard_layout_v<T const volatile>);
+}
+
+struct mixed_access
+{
+public:
+
+    int i;
+    
+protected:
+
+    int j;
+};
+
+struct base
+{
+    int i;
+};
+
+struct derived : base
+{
+    int j;
+};
+
+auto test_main() noexcept -> int
+{
+    // Check that the result is true when the input is a standard layout type.
+    {
+        check_standard_layout<int>();
+        check_standard_layout<double>();
+        check_standard_layout<int*>();
+        check_standard_layout<int const*>();
+        check_standard_layout<char[3]>();
+        check_standard_layout<char(*)[]>();
+        
+        check_standard_layout<base>();
+        check_standard_layout<bmltb::enum_class>();
+        check_standard_layout<bmltb::union_type[2]>();
+        check_standard_layout<int bmltb::class_type::*>();
+        check_standard_layout<bmltb::incomplete_class*>();
+        check_standard_layout<bmltb::incomplete_class*[3][2]>();
+        
+        check_standard_layout<auto (*)(int) noexcept -> void>();
+        check_standard_layout<auto (bmltb::class_type::*)() -> void>();
+        check_standard_layout<auto (bmltb::class_type::*)() const volatile && noexcept -> void>();
+    }
+    
+    // Check that the result is false when the input is not a standard layout type.
+    {
+        check_not_standard_layout<void>();
+        check_not_standard_layout<int&>();
+        check_not_standard_layout<int const&>();
+        check_not_standard_layout<int&&>();
+        check_not_standard_layout<int(&)[]>();
+        
+        check_not_standard_layout<mixed_access>();
+        check_not_standard_layout<derived>();
+        
+        check_not_standard_layout<auto (int) -> void>();
+        check_not_standard_layout<auto (int) const && noexcept -> void>();
+        check_not_standard_layout<auto (&)(int) -> void>();
+        check_not_standard_layout<auto (*&&)(int) noexcept -> void>();
+    }
+
+    return 0;
+}
