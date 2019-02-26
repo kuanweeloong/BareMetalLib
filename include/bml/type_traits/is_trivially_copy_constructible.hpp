5bafa2fb80640d60c5bc3cbd5e4e5efc6f8d26ed
Wee Loong Kuan <kuanweeloong@users.noreply.github.com>
2019-02-27 07:37:28 +0800

    Add is_trivially_copy_constructible

---
+++ include/bml/type_traits/is_trivially_copy_constructible.hpp
@@ -0,0 +1,28 @@
+//
+// Copyright (c) 2019 Wee Loong Kuan
+//
+// This file is licensed under the MIT license. For more details, see the LICENSE.md file in the
+// top-level directory of this distribution.
+//
+
+#pragma once
+#include "is_trivially_constructible.hpp"
+#include "add_lvalue_reference.hpp"
+#include "add_const.hpp"
+
+namespace bml
+{
+    //
+    // See std::is_trivially_copy_constructible.
+    //
+    template <typename T>
+    struct is_trivially_copy_constructible
+        : is_trivially_constructible<T, add_lvalue_reference_ty<add_const_ty<T>>> {};
+
+    //
+    // See std::is_trivially_copy_constructible_v.
+    //
+    template <typename T>
+    inline constexpr auto is_trivially_copy_constructible_v =
+        bool(is_trivially_copy_constructible<T>::value);
+}
