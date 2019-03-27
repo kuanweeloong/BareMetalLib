9aa2cca774c28631a3227cf613dbfd565ac2c14c
Wee Loong Kuan <kuanweeloong@users.noreply.github.com>
2019-03-28 00:33:53 +0800

    Make aligned_storage take ptrdiff_t template arguments

--- include/bml/type_traits/aligned_storage.hpp
+++ include/bml/type_traits/aligned_storage.hpp
@@ -7,15 +7,19 @@
 
 #pragma once
 #include <stddef.h>
+#include "alignment_of.hpp"
 
 namespace bml
 {
     //
-    // See std::aligned_storage.
+    // See std::aligned_storage, except that template arguments are ptrdiff_t instead of size_t.
     //
-    template <::size_t Len, ::size_t Align = alignof(::max_align_t)>
+    template <::ptrdiff_t Len, ::ptrdiff_t Align = alignment_of_v<::max_align_t>>
     struct aligned_storage
     {
+        static_assert(Len >= 0,   "Len cannot be negative.");
+        static_assert(Align >= 0, "Alignment cannot be negative.");
+        
         struct alignas(Align) type
         {
             unsigned char data[Len];
@@ -24,8 +28,8 @@ namespace bml
     
     //
     // See std::aligned_storage_t, except that this is named aligned_storage_ty for POSIX
-    // compatibility.
+    // compatibility, and template arguments are ptrdiff_t instead of size_t.
     //
-    template <::size_t Len, ::size_t Align = alignof(::max_align_t)>
+    template <::ptrdiff_t Len, ::ptrdiff_t Align = alignment_of_v<::max_align_t>>
     using aligned_storage_ty = typename aligned_storage<Len, Align>::type;
 }
