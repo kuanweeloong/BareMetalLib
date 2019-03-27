2dedfa7bdd10ce8bba2f699d8526d993c10a1ef6
Wee Loong Kuan <kuanweeloong@users.noreply.github.com>
2019-03-28 00:56:09 +0800

    Update alignment_of's comments to indicate when program is ill-formed
    This commit adds a note to mention that the program will not compile
    when T's alignment will overflow ptrdiff_t. This is due to the use of
    size_to_ptrdiff when casting alignas' result to ptrdiff_t; a
    static_assert checks for overflow. In reality, this overflow should
    probably not happen - I think compilers should error out on any
    alignment requirement approaching PTRDIFF_MAX.

--- include/bml/type_traits/alignment_of.hpp
+++ include/bml/type_traits/alignment_of.hpp
@@ -13,13 +13,17 @@
 namespace bml
 {
     //
-    // See std::alignment_of, except that this exposes alignment as ptrdiff_t.
+    // See std::alignment_of, except that this exposes alignment as ptrdiff_t. In addition, if the
+    // alignment of T is larger than the maximum value that ptrdiff_t can hold, the program is
+    // ill-formed.
     //
     template <typename T>
     struct alignment_of : integral_constant<::ptrdiff_t, detail::size_to_ptrdiff<alignof(T)>()> {};
 
     //
-    // See std::alignment_of_v, except that this exposes alignment as ptrdiff_t.
+    // See std::alignment_of_v, except that this exposes alignment as ptrdiff_t. In addition, if the 
+    // alignment of T is larger than the maximum value that ptrdiff_t can hold, the program is
+    // ill-formed.
     //
     template <typename T>
     inline constexpr auto alignment_of_v = ::ptrdiff_t(alignment_of<T>::value);
