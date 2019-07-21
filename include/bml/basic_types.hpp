2bf83a187dbd62de7bd0dc5d49cf72b418b2cdd9
Wee Loong Kuan <kuanweeloong@users.noreply.github.com>
2019-07-21 12:33:30 +0800

    Add consolidated basic_types header

---
+++ include/bml/basic_types.hpp
@@ -0,0 +1,14 @@
+//
+// Copyright (c) 2019 Wee Loong Kuan
+//
+// BareMetalLib is based on libc++ (https://libcxx.llvm.org/).
+// 
+// This file is licensed under under the Apache License v2.0 with LLVM Exceptions. For more details,
+// see the LICENSE.md file in the top-level directory of this distribution, or copy at 
+// https://llvm.org/LICENSE.txt.
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+
+#pragma once
+#include "basic_types/nullptr_ty.hpp"
