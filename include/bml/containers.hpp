d23fe5fb927cdf9b87ad98a65d8d5542560992c9
Wee Loong Kuan <kuanweeloong@users.noreply.github.com>
2019-07-24 00:20:41 +0800

    Add consolidated containers header

---
+++ include/bml/containers.hpp
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2019 Wee Loong Kuan
+//
+// BareMetalLib is based on libc++ (https://libcxx.llvm.org/).
+// 
+// This file is licensed under under the Apache License v2.0 with LLVM Exceptions. For more details,
+// see the LICENSE.md file in the top-level directory of this distribution, or copy at 
+// https://llvm.org/LICENSE.txt.
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+
+#pragma once
+#include "containers/in_place.hpp"
+#include "containers/in_place_index.hpp"
+#include "containers/optional.hpp"
+#include "containers/tuple.hpp"
+#include "containers/variant.hpp"
---
+++ tests/test_cases/containers.consolidated_header.cpp
@@ -0,0 +1,44 @@
+//
+// Copyright (c) 2019 Wee Loong Kuan
+//
+// BareMetalLib is based on libc++ (https://libcxx.llvm.org/).
+// 
+// This file is licensed under under the Apache License v2.0 with LLVM Exceptions. For more details,
+// see the LICENSE.md file in the top-level directory of this distribution, or copy at 
+// https://llvm.org/LICENSE.txt.
+//
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+
+#include <bml_testbench.hpp>
+#include <bml/containers.hpp>
+
+auto test_main() noexcept -> int
+{
+	// Check that in_place is available.
+    {
+		static_assert(sizeof(bml::in_place) > 0);
+	}
+	
+	// Check that in_place_index is available.
+    {
+		static_assert(sizeof(bml::in_place_index<2>) > 0);
+	}
+	
+	// Check that optional is available.
+    {
+		static_assert(sizeof(bml::optional<int>) > 0);
+	}
+	
+	// Check that tuple is available.
+    {
+		static_assert(sizeof(bml::tuple<int, char>) > 0);
+	}
+	
+	// Check that variant is available.
+    {
+		static_assert(sizeof(bml::variant<int, char>) > 0);
+	}
+	
+    return 0;
+}
